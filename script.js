4594386c9ac44a70135caa9513a2de3d51130349
Douglas Trindade <douglastrindade22@hotmail.com>
2023-07-12 17:08:14 -0300

    wip: javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+const main = document.querySelector("main");
+const root = document.querySelector(":root");
+const input = document.getElementById("in[ut");
+const resultInput = document.getElementById("result");
+
+const allowedKeys = [
+  "(",
+  ")",
+  "/",
+  "*",
+  "-",
+  "+",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+  "%",
+  " ",
+];
+
+input.addEventListener("keydown");
